7a52c7643f35ab7cb63adcd0d459297c5df445a9
committ <89640845+spaceComplexitys@users.noreply.github.com>
2022-06-28 23:28:11 -0400

    image uplaod

--- src/opend/main.mo
+++ src/opend/main.mo
@@ -1,3 +1,22 @@
+import Principal "mo:base/Principal";
+import NFTActorClass "../nft/nft";
+import Cycles "mo:base/ExperimentalCycles";
+
 actor OpenD {
- 
+
+    public shared(msg) func mint(imgData: [Nat8], name: Text) : async Principal {
+        let owner : Principal = msg.caller;
+
+        Cycles.add(100_500_000_000);
+
+
+        let newNFT = await NFTActorClass.NFT(name, owner, imgData);
+
+        let newCFTPrincipal = await newNFT.getCanisterId();
+
+        return newCFTPrincipal;
+    }
+
 };
+
+
--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../assets/logo.png";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import { Principal } from "@dfinity/principal";
 
-function Item() {
+function Item(props) {
+
+  const [name, setName] = useState();
+  const [ownerId, setOwnerId] = useState();
+  const [image, setImage] = useState();
+  const id = Principal.fromText(props.id);
+
+  const localHost = "http://localhost:8000/";
+  const agent = HttpAgent({host: localHost});
+
+  async function loadNFT() {
+    const NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id,
+    });
+
+    const name = await NFTActor.getName();
+    const ownerId = await NFTActor.getOwner();
+    const imageData = await NFTActor.getAsset();
+    const imageContent = new UInt8Array(imageData);
+
+    const image = URL.createObjectURL(
+      new Blob([imageContent.buffer], { type: "image/png"})
+    );
+
+
+    setName(name);
+    setOwnerId(ownerId.toText());
+    setImage(image);
+  }
+
+
+
+
+  useEffect(() => {
+    loadNFT();
+    loadOwnerId();
+  }, []);
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={image}
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}<span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: {ownerId}
           </p>
         </div>
       </div>
